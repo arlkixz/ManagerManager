699d1bbd3b7ecf778bf43435a28d5edf08f29ba8
arlkixz <WhatisKianoush@proton.me>
2026-05-23 10:11:34 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,6 +2,7 @@
 
 if [ -n "$CHANNEL_NAME" ] && [ -n "$POST_ID" ]; then
     echo "🎯 Starting bot for channel: $CHANNEL_NAME, post: $POST_ID"
+    # اینجا از حالت list استفاده میکنیم و به فایل proxies.txt اشاره میکنیم
     exec python bot.py -c "$CHANNEL_NAME" -pt "$POST_ID" -m list -p proxies.txt
 else
     echo "❌ Please set CHANNEL_NAME and POST_ID in Variables"
