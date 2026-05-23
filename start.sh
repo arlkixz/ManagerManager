0991c819e54f11010b77117ae8aecb21df2ad0b5
arlkixz <WhatisKianoush@proton.me>
2026-05-23 09:43:59 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 
 if [ -n "$CHANNEL_NAME" ] && [ -n "$POST_ID" ]; then
     echo "🎯 Starting bot for channel: $CHANNEL_NAME, post: $POST_ID"
-    exec python bot.py -c "$CHANNEL_NAME" -pt "$POST_ID" -m auto
+    exec python bot.py -c "$CHANNEL_NAME" -pt "$POST_ID" -m list
 else
     echo "❌ Please set CHANNEL_NAME and POST_ID in Variables"
     exec python bot.py --help
